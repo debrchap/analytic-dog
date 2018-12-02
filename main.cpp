08b77a3c12fb4fd1722f4f762318e80ae478c951
debrchap <45127112+debrchap@users.noreply.github.com>
2018-12-02 15:50:57 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,82 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
+#include <string>
+#include "Dog.h"
 using namespace std;
 
 #define SIZE 10
 
-void sortDogs(Dog []);//write this method
+//Function prototypes
+void copyArray();
+void mergeSort();
+void splitMerge();
+void sortDogNames();//write this method
+void sortDogWeights();
+void sortDogColors();
 int searchDog(string name); //write this method
 //it should return index of dog or -1 if not found
-void printDog(Dog); //write this method to print Dog DogInfo
+void printDog(); //write this method to print Dog DogInfo
+//Global variables
+Dog dog_array[SIZE];
+Dog work_array[SIZE];
 
 int main() {
   string name, color;
   int weight;
-  Dog DogsArray[SIZE];
+
   ifstream file("DogInfo.txt");
   if (file)
   {
-    for (int i=0; i < SIZE; i++)
+    for (int k=0; k < SIZE; k++)
     {
       file >> name >> weight >> color;
-      DogsArray[i].setName(name);
-      DogsArray[i].setWeight(weight);
-      DogsArray[i].setColor(color);
+      dog_array[k].setName(name);
+      dog_array[k].setWeight(weight);
+      dog_array[k].setColor(color);
     }
   }
   else exit(-1);
 
-return 0;
+  sortDogNames();
+
+  return 0;
 }
 
-void sortDogs(Dog []) {
+void copyArray() {
+  for (k=0; k < SIZE; k++)
+    work_array[k] = dog_array[k];
+}
+
+void sortDogNames() {
+  //Sort alphabetically by name
+  string temp = "";
+  bool swap;
+  do
+  {
+    swap = false;
+    for (int k=0; k < SIZE; k++)
+    {
+      if (dog_array[k].getName() < dog_array[k+1].getName())
+      {
+        temp = dog_array[k].getName();
+        dog_array[k].setName(dog_array[k+1].getName());
+        dog_array[k+1].setName(temp);
+        swap = true;
+      }
+    }
+  } while (swap);
+
+  for (int k=0; k < SIZE; k++)
+    cout << dog_array[k].getName() << " ";
+}
+
+/*void sortDogWeights(Dog dog_array[]) {
+  //Sort from lightest to heaviest
+}
+
+void sortDogColors(Dog dog_array[]) {
+  //Sort alphabetically by color
   
 }
 
@@ -38,6 +84,6 @@ int searchDog(string name) {
 
 }
 
-void printDog(Dog) {
+void printDog(Dog dog_array[]) {
 
-}
+}*/
