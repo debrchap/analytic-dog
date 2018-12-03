831fa659d785c684385f9569563664921174fe7c
debrchap <45127112+debrchap@users.noreply.github.com>
2018-12-03 06:22:34 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cstdlib>
 #include <fstream>
 #include <string>
 #include "Dog.h"
@@ -10,8 +9,9 @@ using namespace std;
 //Function prototypes
 void sortString(string dog_trait[], int index[]);
 void sortInt(int dog_trait[], int index[]);
-int findDog(string name);
-void printDog();
+//int findDog(string name); //write this method
+//it should return index of dog or -1 if not found
+void printDog(); //write this method to print Dog DogInfo
 //Global variables
 Dog dog_array[SIZE];
 string dog_name[SIZE];
@@ -20,6 +20,7 @@ string dog_color[SIZE];
 int name_index[SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 int weight_index[SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 int color_index[SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int last = SIZE - 1;
 
 int main() {
   string name, color;
@@ -42,14 +43,11 @@ int main() {
       dog_weight[k] = dog_array[k].getWeight();
     for (int k=0; k < SIZE; k++)
       dog_color[k] = dog_array[k].getColor();
-
   }
   else exit(-1);
 
   sortString(dog_name, name_index);
-  sortInt(dog_weight, weight_index);
   sortString(dog_color, color_index);
-
   printDog();
 
   return 0;
@@ -63,9 +61,9 @@ void sortString(string dog_trait[], int index[]) {
   do
   {
     swap = false;
-    for (int k=0; k < SIZE; k++)
+    for (int k=0; k < last; k++)
     {
-      if (dog_trait[k] < dog_trait[k+1])
+      if (dog_trait[k] > dog_trait[k+1])
       {
         temp_trait = dog_trait[k];
         temp_index = index[k];
@@ -76,10 +74,7 @@ void sortString(string dog_trait[], int index[]) {
         swap = true;
       }
     }
-  } while (swap);
-
-  for (int k=0; k < SIZE; k++)
-    cout << dog_trait[k] << " ";
+} while (swap);
 }
 
 void sortInt(int dog_trait[], int index[]) {
@@ -89,9 +84,9 @@ void sortInt(int dog_trait[], int index[]) {
   do
   {
     swap = false;
-    for (int k=0; k < SIZE; k++)
+    for (int k=0; k < last; k++)
     {
-      if (dog_trait[k] < dog_trait[k+1])
+      if (dog_trait[k] > dog_trait[k+1])
       {
         temp_trait = dog_trait[k];
         temp_index = index[k];
@@ -103,13 +98,37 @@ void sortInt(int dog_trait[], int index[]) {
       }
     }
   } while (swap);
-
-  for (int k=0; k < SIZE; k++)
-    cout << dog_trait[k] << " ";
 }
 
 /*int findDog(string name) {
+  int middle, first = 0;
+  bool over = false, found = false;
+  while (!over)
+  {
+    //Every loop, reset middle to be in the middle of the new range
+    middle = (first + last)/2;
 
+    if (nums[middle] == name)
+    {
+      over = true;
+      found = true;
+    }
+    //If user value is less than middle, reset last to one less than the middle that was checked already
+    else if (name < nums[middle])
+      last = middle - 1;
+    //If user value is greater than middle, reset the first to one more than the middle that was checked already
+    else
+      first = middle +1;
+    
+    //If whole array has been searched and no middle value has equaled user value, the value doesn't exist in the array
+    if (first > last)
+      over = true;
+  }
+
+  if (found)
+    cout << "The winning ticket number " << name << " was found on your ticket: " << index[middle] << endl;
+  else
+    cout << "None of your tickets are the winning ticket." << endl;
 }*/
 
 void printDog() {
@@ -120,7 +139,16 @@ void printDog() {
   cout << s <<endl;
   for (int k=0; k < SIZE; k++)
   {
-    name_index[k] = i;
-    cout << dog_array[i].getName << dog_array[i].getWeight << dog_array[i].getColor <<endl;
+    i = name_index[k];
+    cout << i << " " << endl;
+    //cout << dog_array[i].getName << dog_array[i].getWeight << dog_array[i].getColor <<endl;
+  }
+  cout << "List of Dogs, Sorted by Color" << endl;
+  cout << s <<endl;
+  for (int k=0; k < SIZE; k++)
+  {
+    i = color_index[k];
+    cout << i << " " << endl;
+    //cout << dog_array[i].getName << dog_array[i].getWeight << dog_array[i].getColor <<endl;
   }
 }
