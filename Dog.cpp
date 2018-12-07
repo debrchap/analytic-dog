2112a3b53a0d88938411b2711404eefa494d2e33
debrchap <45127112+debrchap@users.noreply.github.com>
2018-12-07 12:56:43 -0800

    Update Dog.cpp

--- Dog.cpp
+++ Dog.cpp
@@ -1,25 +1,25 @@
-//Implementation File for Dog File
-//This is the secret recipe file
+//Implementation File
 #include "Dog.h"
+#include <fstream>
 //default constructor
   Dog::Dog(){
-    name="";
-    weight=0;
-    color="";
+    breed = "";
+    weight = 0;
+    color = "";
   }
 // regular constructor
-  Dog::Dog(string myName,int myWeight,string myColor){
-    name = myName;
+  Dog::Dog(string myBreed, int myWeight, string myColor){
+    breed = myBreed;
     weight = myWeight;
     color = myColor;
   }
 
-  int Dog::getWeight(){
-    return weight;
-  }
-  void Dog::setWeight(int myWeight){
-    weight=myWeight;
-  }
+int Dog::getWeight(){
+  return weight;
+}
+void Dog::setWeight(int myWeight){
+  weight = myWeight;
+}
 
 string Dog::getColor(){
   return color;
@@ -27,9 +27,66 @@ string Dog::getColor(){
 void Dog::setColor(string myColor){
   color = myColor;
 }
-string Dog::getName(){
-  return name;
+string Dog::getBreed(){
+  return breed;
 }
-void Dog::setName(string myName){
-  name = myName;
+void Dog::setBreed(string myBreed){
+  breed = myBreed;
+}
+
+//Global variables
+Dog* dog_ptr = NULL;
+Dog* breed_ptr = NULL;
+Dog* weight_ptr = NULL;
+Dog* color_ptr = NULL;
+int last = SIZE - 1;
+
+void openFile() {
+  string breed, color;
+  int weight;
+
+  dog_ptr = new Dog[SIZE];
+
+  ifstream file("DogInfo.txt");
+  if (file)
+  {
+    for (int k=0; k < SIZE; k++)
+    {
+      file >> breed >> weight >> color;
+      dog_ptr[k].setBreed(breed);
+      dog_ptr[k].setWeight(weight);
+      dog_ptr[k].setColor(color);
+    }
+
+    cout << dog_ptr[2].getBreed() << endl;
+
+    breed_ptr = dog_ptr;
+    weight_ptr = dog_ptr;
+    color_ptr = dog_ptr;
+
+    cout << breed_ptr[2].getBreed() << endl;
+  }
+}
+
+
+void sortBreed() {
+  Dog* temp_ptr;
+  bool swap;
+  do
+  {
+    swap = false;
+    for (int k=0; k < last; k++)
+    {
+      if ((breed_ptr[k].getBreed()).compare(breed_ptr[k+1].getBreed()) > 0)
+      {
+        *temp_ptr = breed_ptr[k];
+        breed_ptr[k] = breed_ptr[k+1];
+        breed_ptr[k+1] = *temp_ptr;
+        swap = true;
+      }
+    }
+  } while (swap);
+  
+  /*for (int k=0; k < SIZE; k++)
+    cout << breed_ptr[k].getBreed() << " " /*<< breed_ptr[k].getWeight << " " << breed_ptr[k].getColor() << endl;*/
 }
